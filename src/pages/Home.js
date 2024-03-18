2a36c3b3c26ae2a735565e4a4a762390951f6446
Hossam630 <H055am@outlook.com>
2024-03-18 06:41:48 +0200

    fix home and about to van btns

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ export default function Home(){
             <div className='content-container'>
             <h1>You got the travel plans, we got the travel vans.</h1>
             <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-            <Link to="/Vans"> <button>find your van</button></Link>
+            <Link to="vans"> <button>find your van</button></Link>
             </div>
         </main>
     )
